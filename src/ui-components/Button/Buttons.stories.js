eab45f8bd63eed117bdb965a54ea58e75aa37346
Felipe <felipemena@outlook.com>
2019-12-14 15:18:40 +1100

    The best button component ever

--- src/ui-components/Button/Buttons.stories.js
+++ src/ui-components/Button/Buttons.stories.js
@@ -1,11 +1,16 @@
-import { boolean, number, text, withKnobs } from "@storybook/addon-knobs";
+import { boolean, number, select, text, withKnobs } from "@storybook/addon-knobs";
 
 import Button from './Button';
+import Colors from '../../design-tokens/colors';
 import React from 'react';
-import { unit } from "../../style-constants";
 
 export default { title: 'Button', decorators: [withKnobs] };
+  
 
-export const withText = () => <div style={{display: 'inline-block', width: unit(13.5)}}>
-    <Button>{text("Text", "Click me")}</Button>
+export const withText = () => 
+ <div style={{ display: 'inline-block', backgroundColor: select('Color',  ['red', 'blue', 'yellow'], 'red', null) }}>
+    <Button>
+        {
+            text("Text", "Click me")
+        }</Button>
 </div>;
