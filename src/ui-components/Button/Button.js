f0b3dfbe7e8cbc588affa5a4084a8d4b6140e695
Carine Oliveira <carineoliveira@Carines-MacBook-Pro.local>
2019-12-14 15:23:18 +1100

    merge

--- src/ui-components/Button/Button.js
+++ src/ui-components/Button/Button.js
@@ -1,14 +1,6 @@
 import {Colors} from '../../design-tokens/colors';
 import React from 'react';
 
-<<<<<<< HEAD
-export default function ({ iconSrc, children }) {
-    return <button>
-        {iconSrc && <img src={iconSrc} />}
-        {children}
-    </button>
-=======
 export default function ({ children, props}) {
     return <button>{children}</button>
->>>>>>> 09c6ad5e2f870d3032f7c83188e6f843926c50ab
 } 
--- src/ui-components/Button/Buttons.stories.js
+++ src/ui-components/Button/Buttons.stories.js
@@ -7,17 +7,10 @@ import React from 'react';
 export default { title: 'Button', decorators: [withKnobs] };
   
 
-<<<<<<< HEAD
-export const withText = () => <div style={{display: 'inline-block', width: unit(13.5)}}>
-    <Button iconSrc="">
-        {text("Text", "Click me")}
-    </Button>
-=======
 export const withText = () => 
  <div style={{ display: 'inline-block', backgroundColor: select('Color',  ['red', 'blue', 'yellow'], 'red', null) }}>
     <Button>
         {
             text("Text", "Click me")
         }</Button>
->>>>>>> 09c6ad5e2f870d3032f7c83188e6f843926c50ab
 </div>;
