1534447e821d4d6aabe4d463744a4f7d5995ebe5
Carine Oliveira <carineoliveira@Carines-MacBook-Pro.local>
2019-12-14 13:43:26 +1100

    label update

--- src/design-tokens/colors.js
+++ src/design-tokens/colors.js
@@ -1,6 +1,6 @@
 export const Colors = {
-    hotRed: rgba(185, 42, 61, 1),
-    warmRed: rgba(240, 52, 82, 1),
+    hotPink: rgba(185, 42, 61, 1),
+    warmPink: rgba(240, 52, 82, 1),
     lightPink: rgba(235, 152, 161, 1),
     babyPink: rgba(251, 192, 204, 1),
     white: rgba(254, 254, 252, 1)
