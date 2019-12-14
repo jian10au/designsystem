1755b7d3162619bf7fe0e6b189bca182e559f996
Felipe <felipemena@outlook.com>
2019-12-14 14:13:32 +1100

    Finished colors stories

--- src/design-tokens/colors.js
+++ src/design-tokens/colors.js
@@ -1,7 +1,6 @@
 export const Colors = {
-    hotPink: rgba(185, 42, 61, 1),
-    warmPink: rgba(240, 52, 82, 1),
-    lightPink: rgba(235, 152, 161, 1),
-    babyPink: rgba(251, 192, 204, 1),
-    white: rgba(254, 254, 252, 1)
+    hotPink: '#B92A3D',
+    warmPink: '#F03452',
+    lightPink: '#EB98A1',
+    babyPink: '#FBC0CC'
 }
--- src/design-tokens/colors.stories.js
+++ src/design-tokens/colors.stories.js
@@ -3,9 +3,26 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 storiesOf('Design tokens', module)
-  .add('Colors', () => <div>
-      <h1>Colors</h1>
-      <p></p>
-      <div>Hot red</div>
-      <div style={{backgroundColor: Colors.hotRed, width: 10, heigth: 10}}></div>
-  </div>);
+    .add('Colors', () => <div>
+        <h1>Colors</h1>
+        <p>
+            <div>Hot Pink</div>
+            <div style={{ background: Colors.hotPink, width: 50, height: 50 }}>
+            </div>
+        </p>
+        <p>
+            <div>Warm Pink</div>
+            <div style={{ background: Colors.warmPink, width: 50, height: 50 }}>
+            </div>
+        </p>
+        <p>
+            <div>Light Pink</div>
+            <div style={{ background: Colors.lightPink, width: 50, height: 50 }}>
+            </div>
+        </p>
+        <p>
+            <div>Baby Pink</div>
+            <div style={{ background: Colors.babyPink, width: 50, height: 50 }}>
+            </div>
+        </p>
+    </div>);
