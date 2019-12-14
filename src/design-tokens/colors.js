6fcf75c6f2ed5ef77249ddd6e70df79d3d107690
Carine Oliveira <carineoliveira@Carines-MacBook-Pro.local>
2019-12-14 13:41:45 +1100

    colors pallete

--- src/design-tokens/colors.js
+++ src/design-tokens/colors.js
@@ -1,3 +1,7 @@
 export const Colors = {
-    hotRed: 'hsl(352%, 63%, 45%)'
+    hotRed: rgba(185, 42, 61, 1),
+    warmRed: rgba(240, 52, 82, 1),
+    lightPink: rgba(235, 152, 161, 1),
+    babyPink: rgba(251, 192, 204, 1),
+    white: rgba(254, 254, 252, 1)
 }
